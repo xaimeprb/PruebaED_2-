8f14fcff25493b2417ac7d89ebb9b9c07e31cbe9
Jaime Pérez-Roget Blanco <xaimeprb@gmail.com>
2025-03-13 18:23:16 +0100

    documentación Curso

--- src/main/java/instituto/Curso.java
+++ src/main/java/instituto/Curso.java
@@ -7,23 +7,46 @@ package instituto;
 import java.util.TreeSet;
 
 /**
- *
- * @author ProfDiurno
+ * La clase Curso representa un curso del instituto.
+ * Contiene el nombre del curso y una lista ordenada de alumnos.
+ * <p>
+ * Permite aniadir alumnos y generar una representación en cadena de texto
+ * con la información del curso.
+ * </p>
+ * 
+ * @author xaime
+ * @version 1.0
+ * @since 2025
  */
 public class Curso {
 
     private String nombre;
     private TreeSet<Persona> listaAlumnos;
 
+    /**
+     * Obtiene el nombre del curso.
+     * 
+     * @return el nombre del curso.
+     */
     protected String getNombre() {
         return nombre;
     }
 
+    /**
+     * Constructor que inicializa el curso con un nombre y una lista vacía de alumnos.
+     * 
+     * @param nombre El nombre del curso.
+     */
     public Curso(String nombre) {
         this.nombre = nombre;
         listaAlumnos = new TreeSet<>();
     }
 
+    /**
+     * Retorna una representación en cadena del curso y sus alumnos.
+     * 
+     * @return Información formateada del curso y su lista de alumnos.
+     */
     @Override
     public String toString() {
         String s = "--------------------" + nombre + "-----------------\n";
@@ -35,6 +58,11 @@ public class Curso {
         return s;
     }
 
+    /**
+     * Añade un alumno a la lista del curso.
+     * 
+     * @param p El objeto Persona a añadir.
+     */
     public void aniadirAlumno(Persona p) {
         listaAlumnos.add(p);
     }
