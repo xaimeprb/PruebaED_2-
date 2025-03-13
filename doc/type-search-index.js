e6e5c1020547ec833e91730b4e3c7876fd953f21
Jaime Pérez-Roget Blanco <xaimeprb@gmail.com>
2025-03-13 18:36:13 +0100

    documentacion generada con JavaDoc

---
+++ doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"instituto","l":"Curso"},{"p":"instituto","l":"Nif"},{"p":"instituto","l":"Persona"}];updateSearchResults();
